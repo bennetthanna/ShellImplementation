a6e7a32b5e6a52954c66ca8a4f46db3a9a18ddaa
Hanna Bennett <bennetthanna22@gmail.com>
2017-02-11 13:15:55 -0800

    Removed unnecessary sandbox files

--- what.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  what.cpp
-//  
-//
-//  Created by Hanna Bennett on 2/10/17.
-//
-//
-
-#include <stdio.h>
--- wtf.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  wtf.cpp
-//  
-//
-//  Created by Hanna Bennett on 2/11/17.
-//
-//
-
-#include <stdio.h>
