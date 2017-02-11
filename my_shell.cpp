07ed088451ea510095a4c03d4f7a916ef07bc639
Hanna Bennett <bennetthanna22@gmail.com>
2017-02-11 14:19:21 -0800

    Fully working shell Can do pipes, redirection, neither, and both combined

--- my_shell.cpp
+++ my_shell.cpp
@@ -8,12 +8,12 @@ int main(int argc, char *argv[])
 {
     char *line = NULL;
     char *token;
-    char **args;
     char **firstArg;
     size_t n = 0;
     int status;
     std::vector <char *> commandArguments;
     std::vector <char *> command;
+    int numPipes;
     
     FILE *fileToBeDuped1;
     int fileDescriptor1;
@@ -47,80 +47,257 @@ int main(int argc, char *argv[])
             token = strtok(NULL, " \n");
         }
         
-        for (int i = 0; i < commandArguments.size(); ++i) {
-            if (strcmp(commandArguments[i], "<") != 0 && strcmp(commandArguments[i], ">") != 0 &&
-                strcmp(commandArguments[i], ">>") != 0 && strcmp(commandArguments[i], "2>") != 0 ) {
-                    command.push_back(commandArguments[i]);
-            } else {
-                break;
+        numPipes = 0;
+        for(int i = 0; i < commandArguments.size(); ++i) {
+            if (strcmp(commandArguments[i], "|") == 0) {
+                ++numPipes;
             }
         }
         
-        firstArg = (char**)malloc(sizeof(char*) * (command.size() + 1));
+        int processCount = numPipes+1;
         
-        for(int i = 0; i < command.size(); ++i) {
-            firstArg[i] = command[i];
-        }
-        firstArg[command.size()] = NULL;
+        std::vector<int *>FDs;
         
-        command.clear();
-        
-        
-        args = (char**)malloc(sizeof(char*) * (commandArguments.size() + 1));
-        
-        for(int i = 0; i < commandArguments.size(); ++i) {
-            args[i] = commandArguments[i];
-        }
-        args[commandArguments.size()] = NULL;
-        
-        for (int i = 0; i < commandArguments.size(); ++i) {
-            if (strcmp(commandArguments[i], "<") == 0 || strcmp(commandArguments[i], ">") == 0 ||
-                strcmp(commandArguments[i], ">>") == 0 || strcmp(commandArguments[i], "2>") == 0 ) {
-                    fileRedirection = true;
-                }
+        for (int i = 0; i < numPipes; ++i) {
+            int *fileDescriptors = new int[2];
+            fileDescriptors[0] = 0;
+            fileDescriptors[1] = 1;
+            FDs.push_back(fileDescriptors);
         }
         
-        switch(fork()) {
-            case -1:
-                perror("Bad Fork");
-                exit(1);
-            case 0:
-                
-                if (fileRedirection == true) {
+        for (int i = 0; i < FDs.size(); ++i) {
+            pipe(FDs[i]);
+        }
+        
+        if (numPipes == 0) {
+            switch(fork()) {
+                case -1:
+                    perror("Bad Fork");
+                    exit(1);
+                case 0:
+                    
                     for (int i = 0; i < commandArguments.size(); ++i) {
-                        if (strcmp(commandArguments[i], "<") == 0) {
-                            fileToBeDuped1 = fopen(commandArguments[i + 1], "r");
-                            fileDescriptor1 = fileno(fileToBeDuped1);
-                            dup2(fileDescriptor1, 0);
-                            fclose(fileToBeDuped1);
-                        } else if (strcmp(commandArguments[i], ">") == 0) {
-                            fileToBeDuped2 = fopen(commandArguments[i + 1], "w");
-                            fileDescriptor2 = fileno(fileToBeDuped2);
-                            dup2(fileDescriptor2, 1);
-                            fclose(fileToBeDuped2);
-                        } else if (strcmp(commandArguments[i], ">>") == 0) {
-                            fileToBeDuped2 = fopen(commandArguments[i + 1], "a");
-                            fileDescriptor2 = fileno(fileToBeDuped2);
-                            dup2(fileDescriptor2, 1);
-                            fclose(fileToBeDuped2);
-                        } else if (strcmp(commandArguments[i], "2>") == 0) {
-                            fileToBeDuped3 = fopen(commandArguments[i + 1], "w");
-                            fileDescriptor3 = fileno(fileToBeDuped3);
-                            dup2(fileDescriptor3, 2);
-                            fclose(fileToBeDuped3);
+                        if (strcmp(commandArguments[i], "<") == 0 || strcmp(commandArguments[i], ">") == 0 ||
+                            strcmp(commandArguments[i], ">>") == 0 || strcmp(commandArguments[i], "2>") == 0 ) {
+                            fileRedirection = true;
                         }
+                    }
+                    
+                    for (int i = 0; i < commandArguments.size(); ++i) {
+                        if (strcmp(commandArguments[i], "<") != 0 && strcmp(commandArguments[i], ">") != 0 &&
+                            strcmp(commandArguments[i], ">>") != 0 && strcmp(commandArguments[i], "2>") != 0 ) {
+                            command.push_back(commandArguments[i]);
+                        } else {
+                            break;
+                        }
+                    }
+                    
+                    firstArg = (char**)malloc(sizeof(char*) * (command.size() + 1));
+                    
+                    for(int i = 0; i < command.size(); ++i) {
+                        firstArg[i] = command[i];
+                    }
+                    firstArg[command.size()] = NULL;
+                    
+                    if (fileRedirection == true) {
+                        for (int i = 0; i < commandArguments.size(); ++i) {
+                            if (strcmp(commandArguments[i], "<") == 0) {
+                                fileToBeDuped1 = fopen(commandArguments[i + 1], "r");
+                                fileDescriptor1 = fileno(fileToBeDuped1);
+                                dup2(fileDescriptor1, 0);
+                                fclose(fileToBeDuped1);
+                            } else if (strcmp(commandArguments[i], ">") == 0) {
+                                fileToBeDuped2 = fopen(commandArguments[i + 1], "w");
+                                fileDescriptor2 = fileno(fileToBeDuped2);
+                                dup2(fileDescriptor2, 1);
+                                fclose(fileToBeDuped2);
+                            } else if (strcmp(commandArguments[i], ">>") == 0) {
+                                fileToBeDuped2 = fopen(commandArguments[i + 1], "r+");
+                                fileDescriptor2 = fileno(fileToBeDuped2);
+                                dup2(fileDescriptor2, 1);
+                                fclose(fileToBeDuped2);
+                            } else if (strcmp(commandArguments[i], "2>") == 0) {
+                                fileToBeDuped3 = fopen(commandArguments[i + 1], "w");
+                                fileDescriptor3 = fileno(fileToBeDuped3);
+                                dup2(fileDescriptor3, 2);
+                                fclose(fileToBeDuped3);
+                            }
+                        }
+                    }
+                    
+                    execvp(firstArg[0], firstArg);
+                    perror("execlp failure");
+                default:
+                    wait(&status);
+                    free(firstArg);
+            }
+        } else {
+            
+            
+            for (int i = 0; i < processCount; ++i) {
+                
+                std::vector<char *>vectorWhat;
+                
+                for(int i = 0; i < commandArguments.size(); ++i) {
+                    if (strcmp(commandArguments[i], "|") != 0) {
+                        vectorWhat.push_back(commandArguments[i]);
+                    } else {
+                        break;
                     }
                 }
                 
-                execvp(firstArg[0], firstArg);
-                perror("execlp failure");
-            default:
+                for (int i = 0; i < commandArguments.size(); ++i) {
+                    if (strcmp(commandArguments[i], "<") == 0 || strcmp(commandArguments[i], ">") == 0 ||
+                        strcmp(commandArguments[i], ">>") == 0 || strcmp(commandArguments[i], "2>") == 0 ) {
+                        fileRedirection = true;
+                    }
+                }
+                
+                std::vector<char *>noFileRedirection;
+                
+                for (int i = 0; i < vectorWhat.size(); ++i) {
+                    if (strcmp(commandArguments[i], "<") != 0 && strcmp(commandArguments[i], ">") != 0 &&
+                        strcmp(commandArguments[i], ">>") != 0 && strcmp(commandArguments[i], "2>") != 0 ) {
+                        noFileRedirection.push_back(commandArguments[i]);
+                    } else {
+                        break;
+                    }
+                }
+                
+                char **whatArgs = (char**)malloc(sizeof(char*) * (noFileRedirection.size() + 1));
+                for(int i = 0; i < noFileRedirection.size(); ++i) {
+                    whatArgs[i] = noFileRedirection[i];
+                }
+                whatArgs[noFileRedirection.size()] = NULL;
+                
+                switch(fork()) {
+                    case -1:
+                        perror("Bad Fork");
+                        exit(1);
+                        
+                    case 0:
+                        if (fileRedirection == true) {
+                            for (int i = 0; i < commandArguments.size(); ++i) {
+                                if (strcmp(commandArguments[i], "<") == 0) {
+                                    fileToBeDuped1 = fopen(commandArguments[i + 1], "r");
+                                    fileDescriptor1 = fileno(fileToBeDuped1);
+                                    dup2(fileDescriptor1, 0);
+                                    fclose(fileToBeDuped1);
+                                } else if (strcmp(commandArguments[i], ">") == 0) {
+                                    fileToBeDuped2 = fopen(commandArguments[i + 1], "w");
+                                    fileDescriptor2 = fileno(fileToBeDuped2);
+                                    dup2(fileDescriptor2, 1);
+                                    fclose(fileToBeDuped2);
+                                } else if (strcmp(commandArguments[i], ">>") == 0) {
+                                    fileToBeDuped2 = fopen(commandArguments[i + 1], "a");
+                                    fileDescriptor2 = fileno(fileToBeDuped2);
+                                    dup2(fileDescriptor2, 1);
+                                    fclose(fileToBeDuped2);
+                                } else if (strcmp(commandArguments[i], "2>") == 0) {
+                                    fileToBeDuped3 = fopen(commandArguments[i + 1], "w");
+                                    fileDescriptor3 = fileno(fileToBeDuped3);
+                                    dup2(fileDescriptor3, 2);
+                                    fclose(fileToBeDuped3);
+                                }
+                            }
+                        }
+                        
+                        if (i == 0) {
+                            for (int j = 0; j < FDs.size(); ++j) {
+                                if (close(FDs[j][0]) == -1) {
+                                    perror("Could not close read");
+                                }
+                                
+                                if (j != 0) {
+                                    if (close(FDs[j][1]) == -1) {
+                                        perror("Could not close write");
+                                    }
+                                }
+                            }
+                            
+                            if (dup2(FDs[i][1], 1) == -1) {
+                                perror("Could not dup2");
+                            }
+                            
+                            execvp(whatArgs[0], whatArgs);
+                            perror("Could not execvp");
+                        }
+                        
+                        if (i > 0 && i < numPipes) {
+                            for (int j =0; j < FDs.size(); ++j) {
+                                
+                                if(j != (i - 1)) {
+                                    if (close(FDs[j][0]) == -1) {
+                                        perror("Could not close write");
+                                    }
+                                }
+                                
+                                if (j != i) {
+                                    if (close(FDs[j][1]) == -1) {
+                                        perror("Could not close read");
+                                    }
+                                }
+                            }
+                            
+                            if (dup2(FDs[i][1], 1) == -1) {
+                                perror("Could not dup2");
+                            }
+                            
+                            if (dup2(FDs[i-1][0], 0) == -1) {
+                                perror("Could not dup2");
+                            }
+                            
+                            execvp(whatArgs[0], whatArgs);
+                            perror("Could not execvp");
+                        }
+                        
+                        if (i == processCount - 1) {
+                            for (int j = 0; j < FDs.size(); ++j) {
+                                if (j != (i - 1)) {
+                                    if (close(FDs[j][0]) == -1) {
+                                        perror("Could not close read");
+                                    }
+                                }
+                                
+                                if (close(FDs[j][1]) == -1) {
+                                    perror("Could not close read");
+                                }
+                            }
+                            
+                            if (dup2(FDs[i-1][0], 0) == -1) {
+                                perror("Could not dup2");
+                            }
+                            
+                            execvp(whatArgs[0], whatArgs);
+                            perror("Could not execvp");
+                        }
+                }
+                
+                if (i != numPipes) {
+                    commandArguments.erase(commandArguments.begin(), (commandArguments.begin() + vectorWhat.size() + 1));
+                } else {
+                    commandArguments.erase(commandArguments.begin(), (commandArguments.begin() + vectorWhat.size()));
+                }
+                
+                vectorWhat.clear();
+                free(whatArgs);
+            }
+            
+            
+            for (int i = 0; i < numPipes; ++i) {
+                if (close(FDs[i][0]) == -1) {
+                    perror("Could not close read");
+                }
+                if (close(FDs[i][1]) == -1) {
+                    perror("Could not close read");
+                }
+            }
+            
+            for (int i = 0; i < processCount; ++i) {
                 wait(&status);
-                free(args);
-                free(firstArg);
+            }
         }
-
-        //clear commandArguments vector
+        
         commandArguments.clear();
         
         //return to command prompt
